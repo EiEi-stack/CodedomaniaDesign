a771e59e50f497cf810e5e0dcba12df182cc94b9
EiEi-stack <yccvenusstudent@gmail.com>
2020-11-26 17:52:44 +0900

    線形レイアウト設計

---
+++ app/src/main/java/et/ad/codedomaniadesign/LinearCalculator.java
@@ -0,0 +1,15 @@
+package et.ad.codedomaniadesign;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LinearCalculator extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_calculator);
+    }
+}
--- app/src/main/java/et/ad/codedomaniadesign/MainActivity.java
+++ app/src/main/java/et/ad/codedomaniadesign/MainActivity.java
@@ -2,13 +2,23 @@ package et.ad.codedomaniadesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonOne = (Button)findViewById(R.id.buttonOne);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LinearCalculator.class);
+                startActivity(intent);
+            }
+        });
     }
 }
